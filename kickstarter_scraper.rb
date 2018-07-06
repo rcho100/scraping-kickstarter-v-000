fc92e1d76d026f31ced1f3bed785bb50c3a485af
Richard Cho <richardcho100@gmail.com>
2018-07-06 20:31:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -28,5 +28,9 @@ require 'nokogiri'
 # end
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 
+  binding.pry
 end
+create_project_hash
